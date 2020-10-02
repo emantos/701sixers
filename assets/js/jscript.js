9e42d07f96db32f4f7304e711071ead775407df4
Guest User <ramdevcalope2015@gmail.com>
2020-10-03 00:06:16 +0800

    first commit

---
+++ assets/js/jscript.js
@@ -0,0 +1,21 @@
+document.getElementById('button').addEventListener('click',
+function(){
+    
+document.querySelector('#popup').style.display = 'flex';
+});
+
+document.getElementById('button2').addEventListener('click',
+function(){
+    
+document.querySelector('#popup').style.display = 'flex';
+});
+
+document.querySelector('.close-btn').addEventListener('click', function(){
+    
+document.querySelector('#popup').style.display = 'none';
+});
+
+document.querySelector('.close').addEventListener('click', function(){
+    
+document.querySelector('#popup').style.display = 'none';
+});
