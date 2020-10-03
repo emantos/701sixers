57f7a4962ca261987c7761287967d8575b3c95fd
johannes-gsi <jmbrothers@yahoo.com>
2020-10-03 15:55:46 +0800

    init reactjs form.io app

---
+++ react-app/src/containers/Footer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <div className="container text-center" style={{padding: '15px'}}>
+          Built with passion by 701Sixers
+          <br />
+          <a href="#">www.701Sixers.com</a>
+        </div>
+      </footer>
+    );
+  }
+}
